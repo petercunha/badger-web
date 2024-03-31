1b9368a6f0d85b85eed47d16ba2500ce2095feee
Peter Cunha <petercunha8@gmail.com>
2024-03-30 21:55:49 -0700

    Mediocre auth because middleware sucks

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,14 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getSession } from './middleware_is_bullshit'
+// import { getSession } from './middleware_is_bullshit'
 
 export async function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get('session')
 
   // Dashboard
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    const session = await getSession(sessionToken?.value)
-    if (!session || new Date(session.expires) < new Date()) {
-      console.log('deleting', session)
-      cookies().delete('session')
-      cookies().delete('user')
+    if (!sessionToken) {
       return NextResponse.redirect(new URL('/', request.url))
     }
   }
@@ -28,3 +24,26 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: '/:path*',
 }
+
+
+// export async function middleware(request: NextRequest) {
+//   const sessionToken = request.cookies.get('session')
+
+//   // Dashboard
+//   if (request.nextUrl.pathname.startsWith('/dashboard')) {
+//     const session = await getSession(sessionToken?.value)
+//     if (!session || new Date(session.expires) < new Date()) {
+//       console.log('deleting', session)
+//       cookies().delete('session')
+//       cookies().delete('user')
+//       return NextResponse.redirect(new URL('/', request.url))
+//     }
+//   }
+
+//   // Home
+//   if (request.nextUrl.pathname === '/') {
+//     if (sessionToken) {
+//       return NextResponse.redirect(new URL('/dashboard', request.url))
+//     }
+//   }
+// }
